1f9b7ab7ecbc0eb4ad947d6ae8c9e89b11ce7a04
Nitin <nsheoran2005@gmail.com>
2023-10-06 10:42:51 +0530

    colorsConst screen create

---
+++ lib/weather/shared/color_const.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorsConst{
+
+  static const bgColor = Colors.white;
+  static const gradientColor = Colors.blue;
+  static const textTileColor = Colors.black54;
+
+}
