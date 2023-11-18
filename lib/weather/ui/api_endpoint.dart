85c05f24b718652d7c0e6a7260a423e0d793d250
Nitin <nsheoran2005@gmail.com>
2023-11-18 07:52:03 +0530

    api endPoint create

---
+++ lib/weather/ui/api_endpoint.dart
@@ -0,0 +1,6 @@
+// class ApiEndPoint {
+//
+// static String baseUrl = 'http://api.openweathermap.org';
+// static String weatherUrl() => '$baseUrl/data/2.5/weather?q=$city&appid=$apiKey';
+// static String apiKey() => 'fe9c862d031a1ffc04111157bb5a3163';
+// }
